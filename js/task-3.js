76a22ef99dd3b92ada5c24d71f7df9e6e89ffb47
MaxBeliienko <142715327+MaxBeliienko@users.noreply.github.com>
2023-12-27 14:50:24 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+"use strict";
+function getElementWidth(content, padding, border) {
+    const numberContent = Number.parseFloat(content);
+    const numberPadding = Number.parseFloat(padding);
+    const numberBorder = Number.parseFloat(border);
+    const overallWidth = numberContent + 2 * (numberPadding + numberBorder);
+    return overallWidth;
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
