88f7de8c59f73343d3632a07c17adf31031e36f0
MaxBeliienko <mbelienko@gmail.com>
2023-12-29 15:32:59 +0200

    hw-06-final

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,39 @@
-"use strict";
-function getElementWidth(content, padding, border) {
-    const numberContent = Number.parseFloat(content);
-    const numberPadding = Number.parseFloat(padding);
-    const numberBorder = Number.parseFloat(border);
-    const overallWidth = numberContent + 2 * (numberPadding + numberBorder);
-    return overallWidth;
+class StringBuilder {
+    #value;
+    constructor(initialValue) {
+        this.#value = initialValue
+    }
+
+    getValue() {
+        return this.#value
+    }
+
+    padEnd(str) {
+        this.#value += str
+    }
+
+    padStart(str) {
+        this.#value = str.concat(this.#value)
+    }
+
+    padBoth(str) {
+        this.#value = `${str}${this.#value}${str}`
+    }
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+
+
+
+
+
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
